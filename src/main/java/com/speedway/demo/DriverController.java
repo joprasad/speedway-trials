71e9360e876ac486eed4d9329cc05af44f2f99e9
Prasad Nettem <rajprasad.nettem@gmail.com>
2021-04-21 05:16:58 +0530

    Green on get and post

--- src/main/java/com/speedway/demo/DriverController.java
+++ src/main/java/com/speedway/demo/DriverController.java
@@ -2,10 +2,7 @@ package com.speedway.demo;
 
 import com.speedway.demo.model.Driver;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,6 +15,12 @@ public class DriverController {
     @ResponseStatus(HttpStatus.CREATED)
     public void addDriver(@RequestBody Driver driver){
         driverList.add(driver);
+
+    }
+
+    @GetMapping("/driver")
+    public List<Driver> getDriver(){
+        return driverList;
 
     }
 }
--- src/test/java/com/speedway/demo/DriverIT.java
+++ src/test/java/com/speedway/demo/DriverIT.java
@@ -1,18 +1,23 @@
 package com.speedway.demo;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jayway.jsonpath.JsonPath;
 import com.speedway.demo.model.Driver;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 @AutoConfigureMockMvc
 @SpringBootTest
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public class DriverIT {
     @Autowired
     MockMvc mockMvc;
@@ -20,11 +25,31 @@ public class DriverIT {
     ObjectMapper objectMapper;
 
     @Test
-    public void addDriverTest() throws Exception{
-        Driver driver=new Driver("Zack","R",30,"John","",1,0);
+    public void addDriverTest() throws Exception {
+        Driver driver = new Driver("Zack", "R", 30, "John", "", 1, 0);
         mockMvc.perform(post("/driver")
                 .contentType(MediaType.APPLICATION_JSON)
-                 .content(objectMapper.writeValueAsString(driver)))
+                .content(objectMapper.writeValueAsString(driver)))
                 .andExpect(status().isCreated());
+    }
+
+    @Test
+    public void getDriverTest() throws Exception {
+        mockMvc.perform(get("/driver"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("length()").value(0));
+    }
+
+    @Test
+    public void postAndGetDriverTest() throws Exception {
+        Driver driver = new Driver("Raj", "R", 33, "John", "", 1, 0);
+        mockMvc.perform(post("/driver")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(driver)))
+                .andExpect(status().isCreated());
+        mockMvc.perform(get("/driver"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("length()").value(1))
+                .andExpect(jsonPath("[0].firstName").value("Raj"));
     }
 }
