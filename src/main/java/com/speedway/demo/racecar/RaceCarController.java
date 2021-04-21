62791899576d2c30d83aab1a1d0f23eee9a9e389
agmothilal <ag.mothilal@gmail.com>
2021-04-21 17:29:30 -0500

    Adding GET race care test case for to verify the one scenario.

--- src/main/java/com/speedway/demo/racecar/RaceCarController.java
+++ src/main/java/com/speedway/demo/racecar/RaceCarController.java
@@ -5,22 +5,25 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/")
 public class RaceCarController {
+    List<RaceCarDTO> raceCars = new ArrayList<>();
 
     @GetMapping("racecars")
     @ResponseStatus(HttpStatus.OK)
     public ResponseDTO getRacecars() {
         return new ResponseDTO(HttpStatus.OK.getReasonPhrase(),
                 HttpStatus.OK.value(),
-                new ArrayList<RaceCarDTO>());
+                raceCars);
     }
 
     @PostMapping("racecar")
     @ResponseStatus(HttpStatus.CREATED)
     public ResponseDTO createRaceCar(@RequestBody RaceCarDTO raceCarDTO) {
+        raceCars.add(raceCarDTO);
         return new ResponseDTO(HttpStatus.CREATED.getReasonPhrase(),
                 HttpStatus.CREATED.value(),
                 "Race car created successfully!");
--- src/test/java/com/speedway/demo/racecar/RaceCarIT.java
+++ src/test/java/com/speedway/demo/racecar/RaceCarIT.java
@@ -8,7 +8,9 @@ import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDoc
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 
 import javax.transaction.Transactional;
 
@@ -18,8 +20,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@Transactional
+//@Transactional
 @AutoConfigureRestDocs
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public class RaceCarIT {
 
     @Autowired
@@ -38,15 +41,37 @@ public class RaceCarIT {
     }
 
     @Test
-    public void createRaceCar() throws Exception {
-        var raceDto = new RaceCarDTO("The Condor", "Corvette", 2019, 27, "AVAILABLE", 189);
+    public void getOneRaceCar() throws Exception {
+        var mvcResult = createRaceCar();
+        mvcResult.andExpect(status().isCreated());
 
-        mockMvc.perform(post("/api/v1/racecar")
-                    .contentType(MediaType.APPLICATION_JSON)
-                    .content(objectMapper.writeValueAsString(raceDto)))
-                .andExpect(status().isCreated())
+        mockMvc.perform(get("/api/v1/racecars"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.status").value("OK"))
+                .andExpect(jsonPath("$.status_code").value(200))
+                .andExpect(jsonPath("$.data.length()").value(1))
+                .andExpect(jsonPath("data[0].nickname").value("The Condor"))
+                .andExpect(jsonPath("data[0].model").value("Corvette"))
+                .andExpect(jsonPath("data[0].year").value(2019))
+                .andExpect(jsonPath("data[0].owner").value(27))
+                .andExpect(jsonPath("data[0].status").value("AVAILABLE"))
+                .andExpect(jsonPath("data[0].top_speed").value(189));
+    }
+
+    @Test
+    public void createRaceCarTest() throws Exception {
+        var mvcResult = createRaceCar();
+        mvcResult.andExpect(status().isCreated())
                 .andExpect(jsonPath("$.status").value("Created"))
                 .andExpect(jsonPath("$.status_code").value(201))
                 .andExpect(jsonPath("$.data").value("Race car created successfully!"));
+    }
+
+    private ResultActions createRaceCar() throws Exception {
+        var raceDto = new RaceCarDTO("The Condor", "Corvette", 2019, 27, "AVAILABLE", 189);
+
+        return mockMvc.perform(post("/api/v1/racecar")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(raceDto)));
     }
 }
